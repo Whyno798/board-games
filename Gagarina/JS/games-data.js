6b497650383040d2da6701f3e56c2cf5ec9ff483
Whyno798 <79536860663r@gmail.com>
2025-06-11 10:25:03 +0300

    Add files via upload

---
+++ Gagarina/JS/games-data.js
@@ -0,0 +1,15 @@
+const games = [
+    {
+        id: 1,
+        title: "Каркассон",
+        players: "2-5",
+        time: "30-60 мин",
+        complexity: 2,
+        type: ["Стратеегия, Евро"],
+        tags: ["Семейная, Развивающая"],
+        image: "imagines\carcassonne_2022_00-1980x1980-wm.jpg"
+    },
+    //докидывай сюда игр
+];
+
+export default games;
